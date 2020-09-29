2c7a09134e8f0256f6bbd4966e34891f9fd8e9ad
François Pluchino <francois.pluchino@klipper.dev>
2020-09-29 16:00:13 +0200

    Override mapping of address fields for partner address model

--- Model/AbstractPartnerAddress.php
+++ Model/AbstractPartnerAddress.php
@@ -21,6 +21,7 @@ use Klipper\Component\Model\Traits\OrganizationalRequiredTrait;
 use Klipper\Component\Model\Traits\TimestampableTrait;
 use Klipper\Component\Model\Traits\UserTrackableTrait;
 use Klipper\Module\PartnerBundle\Model\Traits\PartnerableTrait;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @author François Pluchino <francois.pluchino@klipper.dev>
@@ -34,6 +35,66 @@ abstract class AbstractPartnerAddress implements PartnerAddressInterface
     use EmailableTrait;
     use TimestampableTrait;
     use UserTrackableTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="255")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $street = null;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="255")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $streetComplement = null;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="50")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $postalCode = null;
+
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="128")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $city = null;
+
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="128")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $state = null;
+
+    /**
+     * @ORM\Column(type="string", length=3, nullable=true)
+     *
+     * @Assert\Country
+     * @Assert\Length(max=3)
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $country = null;
 
     /**
      * @ORM\ManyToOne(targetEntity="Klipper\Component\DoctrineChoice\Model\ChoiceInterface")

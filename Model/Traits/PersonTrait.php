8e53651c24ded1514871322dee199adc6dc81f37
François Pluchino <francois.pluchino@klipper.dev>
2020-09-29 15:49:42 +0200

    Add person trait

---
+++ Model/Traits/PersonTrait.php
@@ -0,0 +1,202 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Module\PartnerBundle\Model\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Klipper\Component\DoctrineChoice\Model\ChoiceInterface;
+use Klipper\Component\Phone\Validator\Constraints\Phone as AssertPhone;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+trait PersonTrait
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Klipper\Component\DoctrineChoice\Model\ChoiceInterface")
+     *
+     * @Serializer\Expose
+     */
+    protected ?ChoiceInterface $salutation = null;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="80")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $firstName = null;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="80")
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $lastName = null;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="255")
+     * @Assert\Email
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $email = null;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="80")
+     * @AssertPhone
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $phone = null;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="80")
+     * @AssertPhone
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $mobilePhone = null;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="80")
+     * @AssertPhone
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $fax = null;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="0", max="255")
+     * @Assert\Url
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $websiteUrl = null;
+
+    public function getSalutation(): ?ChoiceInterface
+    {
+        return $this->salutation;
+    }
+
+    public function setSalutation(?ChoiceInterface $salutation): self
+    {
+        $this->salutation = $salutation;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getMobilePhone(): ?string
+    {
+        return $this->mobilePhone;
+    }
+
+    public function setMobilePhone(?string $mobilePhone): self
+    {
+        $this->mobilePhone = $mobilePhone;
+
+        return $this;
+    }
+
+    public function getFax(): ?string
+    {
+        return $this->fax;
+    }
+
+    public function setFax(?string $fax): self
+    {
+        $this->fax = $fax;
+
+        return $this;
+    }
+
+    public function getWebsiteUrl(): ?string
+    {
+        return $this->websiteUrl;
+    }
+
+    public function setWebsiteUrl(?string $websiteUrl): self
+    {
+        $this->websiteUrl = $websiteUrl;
+
+        return $this;
+    }
+}

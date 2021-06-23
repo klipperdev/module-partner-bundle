af455dc6076b779171f5075265d11f61ab7f1714
François Pluchino <francois.pluchino@klipper.dev>
2021-06-23 11:07:32 +0200

    Rename argument of set account method in accountable interface

--- Model/Traits/AccountableInterface.php
+++ Model/Traits/AccountableInterface.php
@@ -25,7 +25,7 @@ interface AccountableInterface
     /**
      * @return static
      */
-    public function setAccount(?AccountInterface $masterAccount);
+    public function setAccount(?AccountInterface $account);
 
     /**
      * @return null|int|string
